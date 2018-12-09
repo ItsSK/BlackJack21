571879b3730802fd6b1e9a1c8c05e8580ae4fe8b
ItsSK <sklassen410@gmail.com>
2018-12-09 02:40:09 -0800

    updates
    implemented - restart_deck
    implemented - restart_hand
    fixed - player input
    fixed - double ACE in hand
    fixed - empty list

--- BlackJack.py
+++ BlackJack.py
@@ -23,7 +23,6 @@ Any hand that goes over 21 "breaks", or is "busted", and is an automatic loser.
 # initialize global variables
 in_play = False
 end_game = False
-message = ""
 outcome = ""
 value_ace = 0
 dealer_score = 0
@@ -73,10 +72,17 @@ class Deck:
             s = s + str(c) + ' '
         return s
 
+    def remove_cards(self):
+        return self.cards.clear()
+
+    def return_cards(self):
+        return self.cards
+
     def shuffle(self):
         random.shuffle(self.cards)
 
     def deal_card(self):
+        global popped
         popped = self.cards.pop(0)
         return popped
 
@@ -96,46 +102,65 @@ class Hand:
         self.player_hand.append(card)
         return self.player_hand
 
+    def remove_cards(self):
+        self.player_hand.clear()
+        return self.player_hand
+
     def get_value(self):
         global value_ace
         value = 0
+        ace_count = 0
         for card in self.player_hand:
             rank = card.get_rank()
             value = value + VALUES[rank]
+            if rank == 'A':
+                ace_count += 1
         value_ace = value
         for card in self.player_hand:
             rank = card.get_rank()
-            if rank == 'A' and value <= 11:
-                value += 10
+            if ace_count < 2:
+                if rank == 'A' and value <= 11:
+                    value += 10
+            else:
+                break
         return value
 
 
 def game_loop():  # What does the player want?
-    global message, player, deck, player_value, dealer_value
+    global player, deck, player_value, hidden_card
     # Game Loop
     while not player_value > 21:
         # does the player have BJ
         if player_value == 21:
             dealer_move()
         else:
-            message = input('Hit or Stand?\n')
-            if message.lower() == 'hit':
-                player.add_card(deck.deal_card())
-                player_value = player.get_value()
-            else:
-                dealer_move()
-        if player_value < 22:
+            message_is_valid = True
+            message = input('Hit or Stand or Exit\n')
+            while message_is_valid:
+                if message.lower() == 'hit':
+                    player.add_card(deck.deal_card())
+                    player_value = player.get_value()
+                    message_is_valid = False
+                elif message.lower() == 'stand':
+                    message_is_valid = False
+                    dealer_move()
+                elif message.lower() == 'exit':
+                    print('####################')
+                    exiting()
+                else:
+                    message = input('Hit or Stand or Exit\n')
+        if player_value < 21:
             print_player()
-            dealer_value = dealer.get_value()
             print('Dealer:', hidden_card, '=', get_hidden_value())
     who_busted()
 
 
 def dealer_move():
     global dealer, dealer_value, player_value, deck, dealer_score
-    # does the dealer have BJ POST REVEAL
+    # dealer draws until hand value > 17
     while not dealer_value > 21:
-        if dealer_value < 17:
+        # TODO possibly create different get_value() methods in Hand()
+        if dealer_value < 17:  # soft 17 ACE and 6 should just be 7 not 17
             dealer.add_card(deck.deal_card())
             dealer_value = dealer.get_value()
         else:
@@ -146,16 +171,28 @@ def dealer_move():
 def compare_values():
     global player_value, dealer_value, player_score, dealer_score
     if player_value > dealer_value:
+        print('\n####################\n')
         print('Player Wins!')
         player_score += 1
     elif player_value < dealer_value:
+        print('\n####################\n')
         print('Dealer Wins!')
         dealer_score += 1
     elif player_value == dealer_value:
+        print('\n####################\n')
         print('Push, Nobody wins')
     print_player()
     print_dealer()
     print('\nHouse:', dealer_score, 'Player:', player_score)
+    print('\n####################')
+    restart_hand()
+
+
+def exiting():
+    # print_player()
+    # print_dealer()
+    print('\nHouse:', dealer_score, 'Player:', player_score)
+    print('\n####################')
     exit(0)
 
 
@@ -163,15 +200,18 @@ def who_busted():
     global player_value, dealer_value, player_score, dealer_score
     # who Busted?
     if player_value > 21:
+        print('\n####################\n')
         print('Player: Busted')
         dealer_score += 1
     elif dealer_value > 21:
         player_score += 1
+        print('\n####################\n')
         print('Dealer: Busted\nYou Win!')
     print_player()
     print_dealer()
     print('\nHouse:', dealer_score, 'Player:', player_score)
-    exit(0)
+    print('\n####################')
+    restart_hand()
 
 
 def print_player():
@@ -206,6 +246,40 @@ def get_hidden_value():
         return str(hidden_card_value)
 
 
+def restart_deck():
+    global deck, player, dealer
+    player.remove_cards()
+    dealer.remove_cards()
+    deck.remove_cards()
+    deck = Deck()
+    player = Hand()
+    dealer = Hand()
+
+
+def restart_hand():
+    global player, dealer, deck, hidden_card, player_value, dealer_value, value_ace
+    if len(deck.return_cards()) < 6:
+        restart_deck()
+    print(len(deck.return_cards()))
+    print('\nHand:')
+    player.remove_cards()
+    dealer.remove_cards()
+    player.add_card(deck.deal_card())  # shown
+    dealer.add_card(deck.deal_card())  # hidden
+    player.add_card(deck.deal_card())  # shown
+    hidden_card = deck.deal_card()  # no hole card
+    dealer.add_card(hidden_card)  # shown
+    player_value = player.get_value()
+    if value_ace == player_value:
+        print('Player:', player, '=', str(player_value))
+    else:
+        # print player value with ace
+        print('Player:', player, '=', str(value_ace) + '/' + str(player_value))
+    dealer_value = dealer.get_value()
+    print('Dealer:', hidden_card, '=', get_hidden_value())
+    game_loop()
+
+
 def start_game():
     global player, dealer, deck, hidden_card, player_value, dealer_value, value_ace
     print("Hand:")
@@ -227,10 +301,9 @@ def start_game():
     print('Dealer:', hidden_card, '=', get_hidden_value())
     game_loop()
 
-# TODO make sure player enters word correctly
-# TODO loop game_loop()
 # TODO multi player mode
 # TODO add betting/money
+# TODO add splitting hand
 
 
 start_game()
--- README.md
+++ README.md
@@ -5,7 +5,6 @@ This is a command line based game of Blackjack.
 ### Warning:  The code in this repository is a work in progress
 
 ### TODO:
-* loop game_loop()
-* make sure player enters word correctly
 * multi player mode
 * add betting/money
+* add splitting hand
